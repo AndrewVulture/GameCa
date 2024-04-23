ed2a3825f4af24a18828a75e386171c5f0abee11
Ethan Kempton <ethankempton0@gmail.com>
2024-04-23 06:29:15 -0700

    photos and stuff

---
+++ userInfo.js
@@ -0,0 +1,31 @@
+const people = [{
+    name: 'Andy May',
+    grade: 12,
+    img: './photos/coolAndyMay.jpg'
+},
+{
+    name: 'Nicholas Inman',
+    grade: 12,
+    img: './photos/nicki.jpg'
+},
+{
+    name: 'Carson Wengert',
+    grade: 11,
+    img: './photos/carson.jpg'
+},
+{
+    name: 'Vaughn Videtic',
+    grade: 12,
+    img: './photos/vaughn.jpg'
+},
+{
+    name: 'Joe Merrill',
+    grade: 12,
+    img: './photos/joe.jpg'
+},
+{
+    name: 'Blane Allen',
+    grade: 12,
+    img: './photos/blane.jpg'
+}
+]
